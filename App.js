8867e2389dc520e376ef8b831a6779921cda051e
Mthssss <matheus.monteiro1b1@gmail.com>
2024-02-22 10:38:53 -0300

    Primeiro Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Matheus Monteiro Rocha</Text>
+      <Text>Desenvolvimento de Dispotivos Moveis</Text>
+      <Text>22/02/2023</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
